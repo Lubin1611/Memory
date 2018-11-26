2e974ad687220b0a06b9ba2426dfc4b9584275ce
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2018-11-26 16:20:35 +0100

    Memory v1

---
+++ java.js
@@ -0,0 +1,32 @@
+var repCartes = ["carte1", "carte2", "carte3", "carte4", "carte5", "carte6", "carte7", "carte8", "carte9", "carte10"];
+
+
+
+var nback = document.getElementById("dosDecarte1");
+
+function change1 () {
+
+   nback.id = "carte1"
+
+    if (nback2.id == "carte2") {
+
+        alert("trouvé");
+
+    }
+
+}
+
+var nback2 = document.getElementById("dosDecarte2");
+
+function change2 () {
+
+   nback2.id = "carte2";
+
+    if (nback.id == "carte1") {
+
+        alert("trouvé");
+
+       }
+
+    }
+
